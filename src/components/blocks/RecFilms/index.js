b6e08c64e48f8fcd928540d255e29f0ac833bfcc
zatoichiii <azovtsevnikita@mail.ru>
2024-04-26 18:17:25 +0300

    ButtonsSort

--- src/components/blocks/RecFilms/index.js
+++ src/components/blocks/RecFilms/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import styles from './RecFilms.module.scss';
 import Container from "../../UI/Container";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 // import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
@@ -12,92 +14,134 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+
+const sort = [
+  { title: 'Комедия', year: 1994 },
+  { title: 'Драма', year: 1972 },
+  { title: 'Детектив', year: 1974 },
+  { title: 'Ужасы', year: 2008 },
+  { title: 'Боевик', year: 1957 },
+  { title: "Фантастика", year: 1993 },
+  { title: 'Мистика', year: 1994 },
+];
+ const year = [
+  {title: '1999'},
+  {title: '2000'},
+  {title: '2001'},
+  {title: '2002'},
+  {title: '2003'},
+  {title: '2004'},
+ ]
+
+ const country = [
+  {title: 'Россия'},
+  {title: 'Германия'},
+  {title: 'Америка'},
+  {title: 'Великобритания'},
+  {title: 'Франция'},
+ ]
 const RecFilms = () => {
-  const [showMenu, setShowMenu] = useState({
-    genres: false,
-    countries: false,
-    years: false,
-    new: false
-  });
-
-  const toggleMenu = (menu) => {
-    setShowMenu(prevState => ({
-      genres: menu === "genres" ? !prevState.genres : false,
-      countries: menu === "countries" ? !prevState.countries : false,
-      years: menu === "years" ? !prevState.years : false,
-      new: menu === "new" ? !prevState.new : false
-    }));
-  };
-
-  const MenuButton = ({ category }) => (
-    <div className={styles.button} onClick={() => toggleMenu(category)}>
-      {category.charAt(0).toUpperCase() + category.slice(1)}
-    </div>
-  );
-
-  const renderMenu = (category) => {
-    if (showMenu[category]) {
-      return (
-        <div className={`${styles.menu} ${styles[category]}`}>
-          {category === "genres" && (
-            <div>
-              <label>
-                <input type="checkbox" value="экшн" /> Экшн
-              </label>
-              <label>
-                <input type="checkbox" value="детектив" /> Детектив
-              </label>
-            </div>
-          )}
-          {category === "countries" && (
-           <div>
-           <label>
-             <input type="checkbox" value="экшн" /> Россия
-           </label>
-           <label>
-             <input type="checkbox" value="детектив" /> Омерика
-           </label>
-         </div>
-          )}
-          {category === "years" && (
-            <div>
-            <label>
-              <input type="checkbox" value="экшн" /> 2007
-            </label>
-            <label>
-              <input type="checkbox" value="детектив" /> 2010
-            </label>
-            {/* Добавь другие жанры */}
-          </div>
-          )}
-          {category === "new" && (
-            <div>
-              {/* Список новых фильмов */}
-            </div>
-          )}
-        </div>
-      );
-    }
-    return null;
-  };
-
-  
-
   return (
     <Container>
       <div className={styles.wrapper}>
         <div className={styles.tittle}>Рекомендации</div>
-        <div className={styles.buttons}>
-          <MenuButton category="genres" />
-          <MenuButton category="countries" />
-          <MenuButton category="years" />
-          <MenuButton category="new" />
+        <div className={styles.sort}>
+    <Autocomplete
+      multiple
+      limitTags={2}
+      id="Janry"
+      options={sort}
+      getOptionLabel={(option) => option?.title || ''}
+      renderInput={(params) => (
+        <TextField {...params} label="Жанры" placeholder="Жанр" />
+      )}
+      sx={{
+        width: '20%',
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&:hover fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+        },
+        '& .MuiInputLabel-root': {
+          color: 'rgb(110, 110, 110)', // Цвет метки
+        },
+        '& .MuiAutocomplete-tag': {
+          backgroundColor: 'rgb(110, 110, 110)', // Цвет тегов выбранных элементов
+        },
+      }}
+    />
+
+<Autocomplete
+      multiple
+      limitTags={1}
+      id="Year"
+      options={year}
+      getOptionLabel={(option) => option?.title || ''}
+      renderInput={(params) => (
+        <TextField {...params} label="Год" placeholder="Год" />
+      )}
+      sx={{
+        width: '10%',
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&:hover fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+        },
+        '& .MuiInputLabel-root': {
+          color: 'rgb(110, 110, 110)', // Цвет метки
+        },
+        '& .MuiAutocomplete-tag': {
+          backgroundColor: 'rgb(110, 110, 110)', // Цвет тегов выбранных элементов
+        },
+      }}
+    />
+
+
+
+<Autocomplete
+      multiple
+      limitTags={2}
+      id="Janry"
+      options={country}
+      getOptionLabel={(option) => option?.title || ''}
+      renderInput={(params) => (
+        <TextField {...params} label="Страны" placeholder="Страна" />
+      )}
+      sx={{
+        width: '13%',
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&:hover fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: 'rgb(110, 110, 110)', // Цвет обводки при фокусировке
+          },
+        },
+        '& .MuiInputLabel-root': {
+          color: 'rgb(110, 110, 110)', // Цвет метки
+        },
+        '& .MuiAutocomplete-tag': {
+          backgroundColor: 'rgb(110, 110, 110)', // Цвет тегов выбранных элементов
+        },
+      }}
+    />
         </div>
-        {renderMenu("genres")}
-        {renderMenu("countries")}
-        {renderMenu("years")}
-        {renderMenu("new")}
-            
+
             <Swiper 
       className={styles.swiper}
       modules={[Navigation, Pagination, Scrollbar, A11y]}
